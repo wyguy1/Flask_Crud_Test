a5164da8184750b2d9bc9e841e4620db2d29034d
RileyWyatt-FS <wlriley1@student.fullsail.edu>
2025-06-13 18:14:51 -0500

    Basically, I wanted to make these bad boys be modular since eventually I wanted to make it modular and scaleable. Hear me out.

--- app/routes/calculator.py
+++ app/routes/calculator.py
@@ -7,6 +7,6 @@ def calculator():
     if request.method == "POST":
         num1 = int(request.form.get("num1", 0))
         num2 = int(request.form.get("num2", 0))
-        result = num1 + num2
-        return render_template("calculator.html", result=result, num1=num1, num2=num2)
-    return render_template("calculator.html")
+        formulaTable(num1, num2)
+    else:
+        return render_template("calculator.html")
