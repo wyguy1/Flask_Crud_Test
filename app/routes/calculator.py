e0ce93e7046637a512a93b26a97417e6e4eef9dd
RileyWyatt-FS <wlriley1@student.fullsail.edu>
2025-06-13 21:49:59 -0500

    Added changes to the devhome html doc.

--- app/routes/calculator.py
+++ app/routes/calculator.py
@@ -7,6 +7,12 @@ def calculator():
     if request.method == "POST":
         num1 = int(request.form.get("num1", 0))
         num2 = int(request.form.get("num2", 0))
-        formulaTable(num1, num2)
+        return formulaTable(num1, num2)
     else:
         return render_template("calculator.html")
+
+def formulaTable(x, y):
+    return exitProcess(x, y, (x + y))
+
+def exitProcess(x, y, answer):
+    return render_template("calculator.html", result=answer, num1=x, num2=y)
